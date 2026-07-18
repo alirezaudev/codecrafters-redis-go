66f949327a792c1aee68e6f0b12922d2ba8ab1f5
Alireza Mousavi <mousaviali1381@gmail.com>
2026-07-18 09:17:59 +0400

    feat: implement SET and GET commands [skip ci]

--- app/main.go
+++ app/main.go
@@ -10,6 +10,7 @@ import (
 	"net"
 	"os"
 	"strconv"
+	"sync"
 )
 
 const (
@@ -25,11 +26,18 @@ const (
 var (
 	crlfBytes = []byte(crlf)
 	ping      = []byte("PING")
-	echo      = []byte("ECHO")
 	pong      = []byte("PONG")
+	echo      = []byte("ECHO")
+	set       = []byte("SET")
+	get       = []byte("GET")
+	ok        = []byte("OK")
+	null      = []byte("$-1\r\n")
 )
 
 var errProtocol = errors.New("protocol violation")
+
+var storage = map[string]string{}
+var mu sync.RWMutex
 
 func main() {
 	l, err := net.Listen("tcp", "0.0.0.0:6379")
@@ -98,6 +106,31 @@ func respond(out []byte, args [][]byte) ([]byte, error) {
 			return out, fmt.Errorf("echo command expected 1 argument, got %d", len(args)-1)
 		}
 		out = appendBulkString(out, args[1])
+
+	case bytes.EqualFold(args[0], set):
+		if len(args) < 3 {
+			return out, fmt.Errorf("set command expected at least 2 arguments, got %d", len(args)-1)
+		}
+
+		mu.Lock()
+		storage[string(args[1])] = string(args[2])
+		mu.Unlock()
+
+		out = appendSimpleString(out, ok)
+
+	case bytes.EqualFold(args[0], get):
+		if len(args) != 2 {
+			return out, fmt.Errorf("get command expected 1 argument, got %d", len(args)-1)
+		}
+
+		mu.RLock()
+		defer mu.RUnlock()
+		if v, exist := storage[string(args[1])]; exist {
+			out = appendBulkString(out, []byte(v))
+			return out, nil
+		}
+
+		return appendNullString(out), nil
 
 	default:
 		return out, fmt.Errorf("unknown command %q", args[0])
@@ -172,6 +205,10 @@ func appendSimpleString(dst, msg []byte) []byte {
 	dst = append(dst, msg...)
 	dst = append(dst, crlf...)
 	return dst
+}
+
+func appendNullString(dst []byte) []byte {
+	return append(dst, null...)
 }
 
 // readDigits parses a decimal number from a "<digits>\r\n" slice without
